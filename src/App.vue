77db9667abc0b6e8e2b0fe3f43eb126d5f70d71f
Rui Vilela <ruivil@gmail.com>
2019-10-26 12:08:55 +0100

    cenas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="font-size:20px;">
+    <!-- v-for directive -->
+    <p v-for="(item, index) in list" :key="index">{{item}}</p>
+    <!-- v-if directive -->
+    <button v-on:click="toggle">Clickame</button>
+    <h1 v-if="isOn">HELLO</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      list: ["Batatas", "Cenouras", "Couves"],
+      isOn: false
+    };
+  },
+  methods: {
+    toggle() {
+      this.isOn = !this.isOn;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
